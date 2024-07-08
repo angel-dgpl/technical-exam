0ea473c653ddd36fc0dc26bc3c189c949a2ce8bd
Angelica Daguplo <angelica.dauplo>
2024-07-08 11:06:00 +0800

    [IMP] removed unnecessary element locators

--- cypress/support/element_locators.js
+++ cypress/support/element_locators.js
@@ -7,8 +7,6 @@ export const elements = {
     select_date: ':nth-child(3) > :nth-child(1) > button > .XQYxZO',
     expected_salary: 'input[name=expected-salary]',
     yrs_exp: '.b33LKJ',
-    radio_yrs_exp: '.action-radios [type="radio"]',
     skills: '.ceZkF0',
-    chck_skills: '.action-checkboxes [type="checkbox"]',
     submit_btn: '.uDW_Qe.wixui-button.PlZyDq'
 }
